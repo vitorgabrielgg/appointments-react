0d92e75596639ae549c44b31ef6539f78b7186b6
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-08-23 10:36:26 -0300

    Refactory methods in Appointments Test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -2,29 +2,29 @@ import React, { act } from "react";
 import ReactDOM from "react-dom/client";
 import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
-  it("renders the customer first name", () => {
-    const customer = { firstName: "Rodolfo" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
     document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
     act(() => {
       ReactDOM.createRoot(container).render(component);
     });
+
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Rodolfo" };
+
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toContain("Rodolfo");
   });
 
   it("renders another customer first name", () => {
     const customer = { firstName: "Jaquile" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
 
-    document.body.replaceChildren(container);
-
-    act(() => {
-      ReactDOM.createRoot(container).render(component);
-    });
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toContain("Jaquile");
   });
