e2e00f1d20343cdf7edb33e2d0dff010445e791d
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-08-23 19:44:13 -0300

    Add appointments list with ol li

--- src/Appointment.jsx
+++ src/Appointment.jsx
@@ -3,3 +3,15 @@ import React from "react";
 export const Appointment = ({ customer }) => {
   return <div className="">{customer.firstName}</div>;
 };
+
+export const AppointmentsDayView = ({ appointments }) => {
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+        {appointments.map((appointment) => (
+          <li key={appointment.startsAt}></li>
+        ))}
+      </ol>
+    </div>
+  );
+};
